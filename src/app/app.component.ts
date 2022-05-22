ce2fafb0f2de8206acd2a6aadf83859e36ec1da9
BhushanBadhe <89174820+BhushanBadhe@users.noreply.github.com>
2022-05-22 21:34:02 +0530

    First and final commit(Custom pipes are not implemented)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  name:string = '';
+  date:string = '';
+  amount:number = null;
+  height: number = null;
+  miles: number = null;
+
+
+  onNameChange(event:Event){
+    this.name = (event.target as HTMLInputElement).value;
+  }
+  onDateChange(event:Event){
+    this.date = (event.target as HTMLInputElement).value;
+  }
+  onAmountChange(event:Event){
+    this.amount  = parseFloat((event.target as HTMLInputElement).value);
+    // this.amount = parseFloat(temp)
+  }
+  onHeightChange(event:Event){
+    this.height  = parseFloat((event.target as HTMLInputElement).value);
+
+  }
+
+  onMilesChange(event:Event){
+    this.miles = parseFloat((event.target as HTMLInputElement).value);
+  }
 }
